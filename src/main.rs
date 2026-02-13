eb646f4a2880a05346672916a8f5e1e706b6b98a
Abreham Kassa <abrehamkassa19@gmail.com>
2026-02-13 16:16:16 +0300

    feat: define ORM models using diesel rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+mod models;
+mod schema;
+
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use models::{Class, Profile, Student};
+use std::env;
+use uuid::Uuid;
+
+use crate::schema::{classes, profiles, students};
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut conn = establish_connection();
+    let user_id = Uuid::now_v7();
+    let class_id = Uuid::now_v7();
+
+    let profile = Profile {
+        id: user_id,
+        first_name: "Abreham".to_string(),
+        last_name: None,
+        username: "UGR/0209/15".to_string(),
+        password_hash: "strong_password_hash".to_string(),
+        img_url: None,
+        role: "student".to_string(),
+    };
+
+    diesel::insert_into(profiles::table)
+        .values(&profile)
+        .execute(&mut conn)
+        .expect("Error saving new user");
+
+    let class = Class {
+        id: class_id,
+        year: 4,
+        section: 2,
+    };
+
+    diesel::insert_into(classes::table)
+        .values(&class)
+        .execute(&mut conn)
+        .expect("Error saving new class");
+
+    let student = Student {
+        id: user_id,
+        class_id: class_id,
+    };
+
+    diesel::insert_into(students::table)
+        .values(&student)
+        .execute(&mut conn)
+        .expect("Error saving new student");
+
+    let student_with_profile = students::table
+        .inner_join(profiles::table)
+        .filter(profiles::id.eq(user_id))
+        .select((Student::as_select(), Profile::as_select()))
+        .load::<(Student, Profile)>(&mut conn)
+        .expect("failed to retrieve data");
+
+    println!("Profile: {:?}", student_with_profile[0].0);
+    println!("User: {:?}", student_with_profile[0].1);
 }
---
+++ src/models.rs
@@ -0,0 +1,67 @@
+use diesel::prelude::*;
+use uuid::Uuid;
+
+#[derive(Queryable, Selectable, Insertable, Debug)]
+#[diesel(table_name = crate::schema::profiles)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Profile {
+    pub id: Uuid,
+    pub first_name: String,
+    pub last_name: Option<String>,
+    pub username: String,
+    pub password_hash: String,
+    pub img_url: Option<String>,
+    pub role: String,
+}
+
+#[derive(Queryable, Selectable, Insertable, Debug)]
+#[diesel(table_name = crate::schema::classes)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Class {
+    pub id: Uuid,
+    pub year: i32,
+    pub section: i32,
+}
+
+#[derive(Queryable, Selectable, Insertable, Debug)]
+#[diesel(table_name = crate::schema::students)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Student {
+    pub id: Uuid,
+    pub class_id: Uuid,
+}
+
+#[derive(Queryable, Selectable, Insertable, Debug)]
+#[diesel(table_name = crate::schema::enrollments)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Enrollment {
+    pub id: Uuid,
+    pub student_id: Uuid,
+    pub course_id: Uuid,
+}
+
+#[derive(Queryable, Selectable, Insertable, Debug)]
+#[diesel(table_name = crate::schema::assignments)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Assignment {
+    pub id: Uuid,
+    pub instructor_id: Uuid,
+    pub class_id: Uuid,
+    pub course_id: Uuid,
+}
+
+#[derive(Queryable, Selectable, Insertable, Debug)]
+#[diesel(table_name = crate::schema::courses)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Course {
+    pub id: Uuid,
+    pub course_id: String,
+    pub name: String,
+}
+
+#[derive(Queryable, Selectable, Insertable, Debug)]
+#[diesel(table_name = crate::schema::instructors)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Instructor {
+    pub id: Uuid,
+}
